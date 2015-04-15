db74f8c8a2f5b2f725cecf9e4bc30a54babc9c3f
davidpazgtz764 <dpaz@Air-de-David.lan>
2015-04-15 12:27:46 -0500

    Se crea el primer release V1.0

--- webserver/grails-app/conf/Config.groovy
+++ webserver/grails-app/conf/Config.groovy
@@ -67,7 +67,8 @@ environments {
     }
     production {
         grails.logging.jul.usebridge = false
-        domainMainPictures = 'http://api.maxibanda.com.mx/pictures/'
+        //domainMainPictures = 'http://api.maxibanda.com.mx/pictures/'
+        domainMainPictures = 'http://ec2-54-148-217-158.us-west-2.compute.amazonaws.com/pictures/'
         // TODO: grails.serverURL = "http://www.changeme.com"
     }
 }
